4ff4f439cccba4f7fb9c5d8f9174e131be572004
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2022-01-16 18:44:42 +0100

    Fix number of harvesters

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 let { harvesterIteration } = require("./creeps.harvester")
 
+const nHarvestersDesired = 4
 
 const main = () => {
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name]
+    const creeps = Object.keys(Game.creeps).map(creepName => Game.creeps[creepName])
+    creeps.forEach(creep => {
         if(creep.memory.role == 'harvester') {
             harvesterIteration(creep)
         }
-    }
+    })
 
-    const spawn = Game.spawns["Spawn1"]
-    spawn.createCreep([MOVE, WORK, WORK, CARRY], undefined, {role: "harvester"})
+    const nHarvestersCurrent = creeps.filter(creep => creep.memory.role == "harvester").length
+    if (nHarvestersCurrent < nHarvestersDesired) {
+        const spawn = Game.spawns["Spawn1"]
+        spawn.createCreep([MOVE, WORK, WORK, CARRY], undefined, {role: "harvester"})
+    }
 }
 
 module.exports.loop = function(){
