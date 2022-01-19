3dec9c00b349f80eb0e85031cb4dbdcf4d8cf225
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2022-01-19 22:01:38 +0100

    Make upgrader gather energy from containers

--- creeps.upgrader.js
+++ creeps.upgrader.js
@@ -3,9 +3,20 @@ const MOVE_OPTIONS = {visualizePathStyle: {stroke: '#ffaa00'}}
 const upgraderIteration = function(creep){
     if(creep.memory.state == "gathering"){
         if(creep.store.getFreeCapacity() > 0) {
+            const notEmptyContainers = creep.room.find(FIND_STRUCTURES)
+                .filter(s => s.structureType == STRUCTURE_CONTAINER)
+                .filter(c => c.store.getUsedCapacity() > 0)
             const source = creep.room.find(FIND_SOURCES)[0]
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, MOVE_OPTIONS)
+            if(notEmptyContainers.length > 0){
+                const notEmptyContainer = notEmptyContainers[0]
+                if(creep.withdraw(notEmptyContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(notEmptyContainer, MOVE_OPTIONS)
+                }
+            }
+            else {
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, MOVE_OPTIONS)
+                }
             }
         }
         else{
