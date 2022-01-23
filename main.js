96e4dea2b2b8eb6c9811b8726e98b6709ac4cbbb
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2022-01-23 14:09:29 +0100

    Make creep names contain role

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let { harvesterIteration } = require("./creeps.harvester")
 let { upgraderIteration } = require("./creeps.upgrader")
 
+const main = () => {
+    removeDeadCreepsFromMemory()
+
+    runCreepIterations()
+
+    ensureNumberOfCreeps()
+}
+
 const removeDeadCreepsFromMemory = () => {
     for(let memCreepName in Memory.creeps){
         if(Game.creeps[memCreepName] == undefined){
@@ -9,12 +17,7 @@ const removeDeadCreepsFromMemory = () => {
     }
 }
 
-const nHarvestersDesired = 4
-const nUpgradersDesired = 1
-
-const main = () => {
-    removeDeadCreepsFromMemory()
-
+const runCreepIterations = () => {
     const creeps = Object.keys(Game.creeps).map(creepName => Game.creeps[creepName])
     creeps.forEach(creep => {
         if(creep.memory.role == 'harvester') {
@@ -24,16 +27,42 @@ const main = () => {
             upgraderIteration(creep)
         }
     })
+}
 
+const ensureNumberOfCreeps = () => {
+    const nHarvestersDesired = 4
+    const nUpgradersDesired = 2
+
+    const creeps = Object.keys(Game.creeps).map(creepName => Game.creeps[creepName])
     const nHarvestersCurrent = creeps.filter(creep => creep.memory.role == "harvester").length
     const nUpgradersCurrent = creeps.filter(creep => creep.memory.role == "upgrader").length
-    const spawn = Game.spawns["Spawn1"]
+    
     if (nHarvestersCurrent < nHarvestersDesired) {
-        spawn.createCreep([MOVE, WORK, WORK, CARRY], undefined, {role: "harvester", state: "gathering"})
+        createCreep("harvester")
     }
     else if (nUpgradersCurrent < nUpgradersDesired) {
-        spawn.createCreep([MOVE, WORK, WORK, CARRY], undefined, {role: "upgrader", state: "gathering"})
+        createCreep("upgrader")
     }
+}
+
+const createCreep = (role) => {
+    const spawn = Game.spawns["Spawn1"]
+    
+    const creepUUID = createUUID()
+    const creepName = `${role}${creepUUID}`
+
+    spawn.createCreep([WORK, WORK, MOVE, CARRY], creepName, 
+        {role, state: "gathering"}
+    )
+}
+
+const createUUID = () => {
+    const timeMs = new Date().getTime()
+    const uuid = 'xxxxx'.replace(/[x]/g, (c) => {
+        const r = Math.floor((timeMs + Math.random()*16)%16)
+        return r.toString(16)
+    })
+    return uuid
 }
 
 module.exports.loop = function(){
